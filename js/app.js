f052ee2dcb37bf62cc361390ed6364cab733e3f0
mike00151 <mike00151@gmail.com>
2019-11-29 00:50:28 +0800

    update

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
 function play(hz, type, seconds) {
-    var osc = new Tone.Oscillator(hz, type).toMaster().start().stop("+5");
+    
+    var merge = new Tone.Merge().toMaster();
+
+    var osc1 = new Tone.Oscillator(1, "square").connect(merge.left);
+    var osc2 = new Tone.Oscillator(5, "square").connect(merge.right);
+
+    osc1.start().stop("+10");
+    osc2.start().stop("+10");
+
 }
