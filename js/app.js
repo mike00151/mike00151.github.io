619dd6927a51c045b9f20ad43ac3ace51ffc7683
unknown <mike00151@gmail.com>
2019-12-03 20:04:57 +0800

    update

--- js/app.js
+++ js/app.js
@@ -1,11 +1,25 @@
-function play(hz, type, seconds) {
-    
+function playPerSet(content) {
+
     var merge = new Tone.Merge().toMaster();
 
-    var osc1 = new Tone.Oscillator(1, "square").connect(merge.left);
-    var osc2 = new Tone.Oscillator(5, "square").connect(merge.right);
+    var osc1 = new Tone.Oscillator(content.frequency1, content.wave).connect(merge.left);
+    var osc2 = new Tone.Oscillator(content.frequency2, content.wave).connect(merge.right);
 
-    osc1.start().stop("+10");
-    osc2.start().stop("+10");
+    osc1.start().stop(content.timeInSeconds);
+    osc2.start().stop(content.timeInSeconds);
 
+    // Wait for the end.
+    while (osc1.state == "started" && osc2.state == "started") {
+        sleep(500);
+    }
+
+    return;
+
+}
+
+function sleep(milliseconds) {
+    var start = new Date().getTime();
+    while (1)
+        if ((new Date().getTime() - start) > milliseconds)
+            break;
 }
