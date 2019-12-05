00960c756368d800e955bfb0aecb98c3e0d2a2e1
mike00151 <mike00151@gmail.com>
2019-12-05 21:54:07 +0800

    add css

--- js/app.js
+++ js/app.js
@@ -1,20 +1,20 @@
 function playPerSet(content) {
+    document.getElementById("playing").innerHTML = "正在執行 f1 : " + content.frequency1 + " f2 : " + content.frequency2 + " wave : " + content.wave;
 
+    for (i = 0; i < content.timeInSeconds; i++) {
+        playPerSecond(content.frequency1, content.frequency2, content.wave);
+        sleep(1000);
+    }
+}
+
+function playPerSecond(f1, f2, wave){
     var merge = new Tone.Merge().toMaster();
 
-    var osc1 = new Tone.Oscillator(content.frequency1, content.wave).connect(merge.left);
-    var osc2 = new Tone.Oscillator(content.frequency2, content.wave).connect(merge.right);
+    var osc1 = new Tone.Oscillator(f1, wave).connect(merge.left);
+    var osc2 = new Tone.Oscillator(f2, wave).connect(merge.right);
 
-    osc1.start().stop(content.timeInSeconds);
-    osc2.start().stop(content.timeInSeconds);
-
-    // Wait for the end.
-    while (osc1.state == "started" && osc2.state == "started") {
-        sleep(500);
-    }
-
-    return;
-
+    osc1.start().stop("+1");
+    osc2.start().stop("+1");
 }
 
 function sleep(milliseconds) {
